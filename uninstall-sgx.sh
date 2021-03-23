10ad9f5e01db60fe126bf478cedda24abf774e04
shok1122 <shok1122@zohomail.com>
2021-03-23 14:53:37 +0900

    Added a code for removing files and repositories

--- uninstall-sgx.sh
+++ uninstall-sgx.sh
@@ -12,11 +12,34 @@ if [ "root" != "$USER" ]; then
     exit 0
 fi
 
-banner "Stopping aesmd service"
+PATH_SGX_DRIVER="/lib/modules/$(uname -r)/kernel/drivers/intel/sgx"
+
+banner "Stopping an aesmd service"
 systemctl stop aesmd
 
-banner "Disabling aesmd service"
+banner "Disabling an aesmd service"
 systemctl disable aesmd
 
-banner "Removing isgx module"
+banner "Removing an isgx module"
 /sbin/modprobe --remove isgx
+rm -rf $PATH_SGX_DRIVER
+sed '/^isgx$/d' /etc/modules
+
+banner "Removing a linux-sgx-driver repository"
+rm -rf linux-sgx-driver
+
+banner "Removing a sgx sdk"
+rm -rf /opt/intel
+sed '/^source /opt/intel/sgxsdk/environment$/d' ~/.bashrc
+
+banner "Removing a linux-sgx repository"
+rm -rf linux-sgx
+
+banner "Removing a distro file"
+rm -f cache/distro
+
+banner "Removing toolsets"
+rm -f /usr/local/bin/as
+rm -f /usr/local/bin/ld
+rm -f /usr/local/bin/ld.gold
+rm -f /usr/local/bin/objdump
