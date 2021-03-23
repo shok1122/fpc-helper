411591988edb7f81c9b3e25440c6710e33ff9c7d
shok1122 <shok1122@zohomail.com>
2021-03-23 23:10:19 +0900

    Added cloning the fabric repository and fabric-private-chaincode repository

--- install-fpc.sh
+++ install-fpc.sh
@@ -6,6 +6,8 @@ GO_VERSION='1.15.10'
 DOCKER_VERSION='5:20.10.5~3-0~ubuntu-bionic'
 DOCKER_COMPOSE_VERSION='1.28.5'
 PROTOC_VERSION='3.11.4'
+FABRIC_VERSION='v2.3.0'
+FABRIC_PRIVATE_CHAINCODE_VERSION='v1.0-rc1'
 
 banner()
 {
@@ -32,7 +34,8 @@ apt install -y \
     curl \
     lsb-release \
     jq \
-    basez
+    basez \
+    clang-format
 
 RET=0
 which go > /dev/null || RET=1
@@ -96,7 +99,7 @@ if [ -z "$NANOPB_PATH" ] || [ ! -f $NANOPB_PATH/generator/proto/nanopb_pb2.py ];
         mkdir -p $NANOPB_PATH
         cd $NANOPB_PATH
         pwd
-        git clone https://github.com/nanopb/nanopb.git -b nanopb-0.4.3 $NANOPB_PATH
+        git clone https://github.com/nanopb/nanopb.git -b nanopb-0.4.3 --depth 1 $NANOPB_PATH
         cd generator/proto
         make
     )
@@ -116,14 +119,25 @@ else
     echo "DONE."
 fi
 
-exit 0
+banner "Cloning fabric ($FABRIC_VERSION)"
+FABRIC_PATH=$GOPATH/src/github.com/hyperledger/fabric
+if [ ! -d $FABRIC_PATH ]; then
+    git clone https://github.com/hyperledger/fabric.git -b $FABRIC_VERSION --depth 1 $FABRIC_PATH
+else
+    echo "DONE."
+fi
 
-banner 'Cloning fabric-private-chaincode'
-git clone https://github.com/hyperledger-labs/fabric-private-chaincode -b v1.0-rc1
+banner "Cloning fabric-private-chaincode ($FABRIC_PRIVATE_CHAINCODE_VERSION)"
+FABRIC_PRIVATE_CHAINCODE_PATH=$GOPATH/src/github.com/hyperledger-labs/fabric-private-chaincode
+if [ ! -d $FABRIC_PRIVATE_CHAINCODE_PATH ]; then
+    git clone --recursive https://github.com/hyperledger-labs/fabric-private-chaincode -b $FABRIC_PRIVATE_CHAINCODE_VERSION --depth 1 $FABRIC_PRIVATE_CHAINCODE_PATH
+else
+    echo "DONE."
+fi
 
 # setup SPID
 banner 'Setup SGX Attestation Service'
-PATH_API_KEY=$WORKSPACE/fabric-private-chaincode/config/ias/api_key.txt
+PATH_API_KEY=$FABRIC_PRIVATE_CHAINCODE_PATH/config/ias/api_key.txt
 if [ ! -f "$PATH_API_KEY" ];
     echo -n "API_KEY: "
     read API_KEY
@@ -133,7 +147,7 @@ echo $PATH_API_KEY
 echo -n "> "
 cat  $PATH_API_KEY
 
-PATH_SPID=$WORKSPACE/fabric-private-chaincode/config/ias/spid.txt
+PATH_SPID=$FABRIC_PRIVATE_CHAINCODE_PATH/config/ias/spid.txt
 if [ ! -f "$PATH_SPID" ];
     echo -n "SPID: "
     read SPID
@@ -143,7 +157,7 @@ echo $PATH_SPID
 echo -n "> "
 cat  $PATH_SPID
 
-PATH_SPID_TYPE=$WORKSPACE/fabric-private-chaincode/config/ias/spid_type.txt
+PATH_SPID_TYPE=$FABRIC_PRIVATE_CHAINCODE_PATH/config/ias/spid_type.txt
 if [ ! -f "$PATH_SPID_TYPE" ];
     echo "epid-unlinkable" > $WORKSPACE/fabric-private-chaincode/config/ias/spid_type.txt
 fi
@@ -151,4 +165,10 @@ echo $PATH_SPID_TYPE
 echo -n "> "
 cat  $PATH_SPID_TYPE
 
-# creation of docker images
+banner "Compiling fabric-private-chaincode ($FABRIC_PRIVATE_CHAINCODE_VERSION)"
+(
+    cd $FABRIC_PRIVATE_CHAINCODE_PATH
+    make
+)
+
+
