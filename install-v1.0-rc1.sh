155f49acc36b342363863415e804acb7273dd433
shok1122 <shok1122@zohomail.com>
2021-03-23 21:17:58 +0900

    Fixed typo

--- install-v1.0-rc1.sh
+++ install-v1.0-rc1.sh
@@ -53,7 +53,7 @@ if [ 1 = $RET ]; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
         "deb [arch=amd64 signed-by=/usr/share/eyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-        > /etc/apt/sources.list.d/ocker.list
+        > /etc/apt/sources.list.d/docker.list
     apt update
     apt install -y \
         docker-ce=${DOCKER_VERSION} \
